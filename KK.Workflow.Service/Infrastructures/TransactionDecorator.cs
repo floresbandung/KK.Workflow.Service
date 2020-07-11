4558c32e5a6343d3ce9859137321dc1bd9957b86
kunkun kurniawan <kurnia.wirawan@gmail.com>
2020-07-11 13:30:31 +0700

    add transaction decorator

---
+++ KK.Workflow.Service/Infrastructures/TransactionDecorator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using KK.Workflow.Service.DataContext;
+using MediatR;
+
+namespace KK.Workflow.Service.Infrastructures
+{
+    public class TransactionDecorator<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly WorkflowDataContext _dataContext;
+
+        public TransactionDecorator(WorkflowDataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            await using var transaction = await _dataContext.Database.BeginTransactionAsync(cancellationToken);
+            try
+            {
+                var response = await next();
+                await transaction.CommitAsync(cancellationToken);
+                return response;
+            }
+            catch (Exception)
+            {
+                await transaction.RollbackAsync(cancellationToken);
+                throw;
+            }
+        }
+    }
+}
